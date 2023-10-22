4d4a565ac63481f102a1abbaa193da5cb4bbe5b5
Furai-Remix <furairemix@gmail.com>
2023-10-22 20:02:05 -0300

    22/10 20:01Hs

---
+++ java/java.js
@@ -0,0 +1,9 @@
+var nombre=document.getElementById([id=nombre])
+var email=document.getElementById([id=email])
+var TELEFONO=document.getElementById([id=TELEFONO])
+var ENVIAR=document.getElementById([id=ENVIAR])
+var CANCELAR=document.getElementById([id=CANCELAR])
+
+if (nombre.value.lenght > 2){ document.write ("Hola")}
+
+
