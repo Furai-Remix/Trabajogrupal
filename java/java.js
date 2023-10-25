8e0c4ec079f0d4b3910a7b442e26db583d40d375
felix. <felipealmiron060@gmail.com>
2023-10-25 20:41:01 -0300

    25/10 20:40

--- java/java.js
+++ java/java.js
@@ -1,9 +1,44 @@
-var nombre=document.getElementById([id=nombre])
-var email=document.getElementById([id=email])
-var TELEFONO=document.getElementById([id=TELEFONO])
-var ENVIAR=document.getElementById([id=ENVIAR])
-var CANCELAR=document.getElementById([id=CANCELAR])
 
-if (nombre.value.lenght > 2){ document.write ("Hola")}
+const $nombre = document.getElementById("nombre"),
+      $email = document.getElementById("email"),
+      $TELEFONO = document.getElementById("TELEFONO"),
+      $ENVIAR = document.getElementById("ENVIAR");
+      
 
 
+      function validateInputs(){
+        const nombreregex =  /A-Za-záéíóúÁÉÍÓÚñÑ\s/;
+
+        const emailregex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+
+        const TELEFONOregex = /\d{3}-\d{3}-\d{4}/;
+
+
+        if ($nombre.value === ""){
+            alert("No dejar el campo de Nombre vacio")
+        }
+        if ($email.value === ""){
+            alert("No dejar el campo de Email vacio")
+        }    
+        if ($TELEFONO.value === ""){
+            alert("No dejar el campo del Telefono vacio")
+        }
+
+        if (!nombreregex.test(nombre.value)){
+            alert("Ingrese un formato correcto de Nombre")
+        }
+        if (!emailregex.test(email.value)){
+            alert("Ingrese un formato correcto de Email")
+        }
+        if (!TELEFONOregex.test(TELEFONO.value)){
+            alert("Ingrese un formato correcto de Telefono")
+        }
+
+      }
+
+      document.addEventListener("click", (e) => {
+        if (e.target === $ENVIAR) {
+            e.preventDefault();
+            validateInputs();
+        }
+      })
