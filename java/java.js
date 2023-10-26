c6bb020820c9d41b2f288299d424c4700140900e
Furai-Remix <furairemix@gmail.com>
2023-10-25 21:23:40 -0300

    25/10 21:23Hs

--- java/java.js
+++ java/java.js
@@ -11,7 +11,7 @@ const $nombre = document.getElementById("nombre"),
 
         const emailregex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
 
-        const TELEFONOregex = /^\d{3}-\d{3}-\d{4}$/;
+        const TELEFONOregex = /[d{3}-\d{3}-\d{4}]/;
 
 
         if ($nombre.value === ""){
